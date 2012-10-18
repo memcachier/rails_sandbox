41e5115d3c765f6bcd9f8f5e762b05c9fefc0488
Alex Loddengaard <alexloddengaard@gmail.com>
2012-10-18 10:33:12 -0700

    Empty rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsSandbox::Application.config.secret_token = 'f22b3e4d1b854dc8a73e61208ec4839ab17029f4e7a874a715cea8c768ddf99dfe2df1be5458fb046a8d3ccddc09af21c164bda67c9eb3b952571393829b068a'
